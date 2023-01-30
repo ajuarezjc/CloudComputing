1fc0e3e371613a17496fecd67016a212e3e4f194
ajuarezjc <ajuarez.isd@gmail.com>
2023-01-29 18:58:39 -0600

    Adding P1-SumaArreglosParalela

---
+++ SumaArreglosParalela.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <omp.h>
+#define N 100000
+#define chunk 250
+#define mostrar 15
+#define salto 150
+
+void imprimeArreglo (float *d);
+
+int main() {
+    std::cout << "Sumando Arreglos en Paralelo! \n";
+    float a[N], b[N], c[N];
+    int i;
+
+    for (i = 0; i < N; i++)
+    {
+        a[i] = i * 10;
+        b[i] = (i + 3) * 2.5;
+    }
+
+    int pedazos = chunk;
+    
+    #pragma omp parallel for shared(a, b,c, pedazos) private(i) schedule(static, pedazos)
+
+    for (i = 0; i < N; i++)
+        c[i] = a[i] + b[i];
+
+    std:: cout << "Imprimiendo cada " << salto << " los primeros " << (mostrar * salto) << " valores del arreglo a: " << std::endl;
+    imprimeArreglo(a);
+    std:: cout << "Imprimiendo cada " << salto << "  los primeros " << (mostrar * salto) << " valores del arreglo b: " << std::endl;
+    imprimeArreglo(b);
+    std:: cout << "Imprimiendo cada " << salto << "  los primeros " << (mostrar * salto) << " valores del arreglo c: " << std::endl;
+    imprimeArreglo(c);
+}
+
+void imprimeArreglo (float *d) {
+    for (int x = 0; x < (mostrar * salto); x=x+salto)
+        std::cout << d[x] << " - ";
+    std::cout << std::endl;
+}
